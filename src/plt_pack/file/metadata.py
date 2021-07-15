e2d340ecdf7263009c56152e645c9dbda4a8122b
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2021-07-15 21:28:13 +0200

    deleted print

--- src/plt_pack/file/metadata.py
+++ src/plt_pack/file/metadata.py
@@ -7,7 +7,3 @@ def get_metadata() -> dict:
     py_version = sys.version
     time = dt.utcnow().strftime('%d %B %Y %H:%M:%S')
     return dict(time=time, py_version=py_version, platform=platform)
-
-
-if __name__ == '__main__':
-    print(get_metadata())
