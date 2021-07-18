2d072dbeb11f047eba5908d80504d9e5b213acc2
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2021-07-18 21:38:08 +0200

    use save_opts

--- src/plt_pack/project_utils/plt_project.py
+++ src/plt_pack/project_utils/plt_project.py
@@ -1,79 +1,121 @@
+import logging
 from pathlib import Path
 from datetime import datetime as dt
 from functools import wraps
-from typing import List, Dict, Any
+from typing import List, Dict, Tuple, Any, Optional
 from types import FunctionType
 from contextlib import contextmanager
 
+import matplotlib.pyplot as plt
+
+from .save_opts import SaveOpts, PLT_FORMAT
+from .registered_func import RegisteredFunc
 from ..file import save_plt_file, read_plt_file, PltFile
 
 
 class PltProject(object):
     _TEST: bool = False
 
-    def __init__(self, folder: Path or str, datefmt: str = '%d-%b-%H-%M-%S', format: str = 'plt'):
+    def __init__(self,
+                 folder: Path or str,
+                 save_opts: SaveOpts = None,
+                 **save_opt_kwargs,
+                 ):
         self.folder = Path(folder)
         self.folder.mkdir(exist_ok=True)
-        self.datefmt = datefmt
-        self.format = format
-        self.registered_functions: Dict[str, ]
+
+        self.default_opts = save_opts or SaveOpts(**save_opt_kwargs)
+        self.current_opts: SaveOpts = None
+        self.registered_functions: Dict[str, RegisteredFunc] = {}
+
+    def _get_save_opts(self, **kwargs) -> SaveOpts:
+        opts = self.current_opts or self.default_opts
+        return opts.new_opts(**kwargs)
 
     @contextmanager
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self,
+                 save_opts: SaveOpts = None,
+                 **save_opt_kwargs
+                 ):
 
-    def save(self, func,
-             args: tuple = (),
-             kwargs: dict = None,
-             func_name: str = None,
-             rewrite: bool = False) -> Path:
-        func_name = func_name or func.__name__
-        file_path = self.folder / _func_name(func_name, self.datefmt, rewrite, self.format)
-        save_plt_file(file_path, func, *args, **(kwargs or {}))
-        return file_path
+        self.current_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
+        try:
+            yield
+        finally:
+            self.current_opts = None
 
-    def auto_save(self, name: str = None, rewrite: bool = False):
+    def save(self,
+             func: FunctionType,
+             args: Optional[Tuple[Any, ...]],
+             kwargs: Optional[Dict[str, Any]] = None,
+             save_opts: Optional[SaveOpts] = None,
+             **save_opt_kwargs,
+             ) -> Path:
+
+        save_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
+
+        plt_name, fig_name = save_opts.get_names(func)
+        plt_path = self.folder / plt_name
+
+        kwargs = kwargs or {}
+
+        if save_opts.save_figure:
+            _save_figure(func, self.folder / fig_name, args, kwargs)
+
+        if save_opts.save_plt:
+            save_plt_file(plt_path, func, *args, **kwargs)
+
+        return plt_path
+
+    def auto_save(self, save_opts: Optional[SaveOpts] = None, **save_opt_kwargs):
+        save_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
+
         def decorator(func):
             @wraps(func)
             def wrapper(*args, **kwargs):
                 if not self._TEST:
                     func(*args, **kwargs)
-                self.save(func, args=args, kwargs=kwargs, func_name=name, rewrite=rewrite)
+                self.save(func, args, kwargs, save_opts)
 
             return wrapper
+
         return decorator
 
     def register(self, func: FunctionType):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if not self._TEST:
-                func(*args, **kwargs)
             self._register_called(func, args, kwargs)
+
         return wrapper
 
     def _register_called(self, func: FunctionType, args: List[Any], kwargs: Dict[str, Any]):
-        pass
+        func(*args, **kwargs)
+        self.save(func, args, kwargs)
 
     def list_files(self) -> List[str]:
-        return [path.stem for path in self.folder.glob(f'*.{self.format}')]
+        return [path.stem for path in self.folder.glob(f'*.{PLT_FORMAT}')]
 
     def load_file(self, name: str) -> PltFile:
-        return read_plt_file(self.folder / f'{name}.{self.format}')
+        return read_plt_file(self.folder / f'{name}.{PLT_FORMAT}')
 
     def __getitem__(self, name: str):
         return self.load_file(name)
 
-    @property
-    def time_str(self):
-        return time_str(self.datefmt)
 
+def _save_figure(
+        func: FunctionType,
+        path: Path,
+        args: Tuple[Any, ...],
+        kwargs: Dict[str, Any]
+):
 
-def _func_name(func_name: str, datefmt: str, rewrite: bool, format: str = 'plt'):
-    if rewrite:
-        return f'{func_name}.{format}'
-    else:
-        return f'{func_name}-{time_str(datefmt)}.{format}'
-
-
-def time_str(datefmt: str):
-    return dt.now().strftime(datefmt)
+    path = str(path.absolute())
+    plt.ioff()
+    try:
+        func(*args, **kwargs)
+    except Exception as err:
+        logger = logging.getLogger(__name__)
+        logger.exception(err)
+        logger.error(f'Function execution failed. Could not save to {path}.')
+    plt.savefig(path)
+    plt.ion()
