3281fee33ab7f0ca54ae133c50923c6ee3390412
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2021-07-21 15:12:39 +0200

    minor refactoring

--- tests/fixtures/func_with_many_subfunctions.py
+++ tests/fixtures/func_with_many_subfunctions.py
@@ -95,7 +95,7 @@ def colorbar(mappable):
     ax = mappable.axes
     fig = ax.figure
     divider = make_axes_locatable(ax)
-    cax = divider.append_axes("right", size="5%", pad=0.05)
+    cax = divider.append_axes('right', size='5%', pad=0.05)
     cbar = fig.colorbar(mappable, cax=cax)
     plt.sca(last_axes)
     return cbar
