133bc5e19e10f916e38ba99fa77f2fd5322ae28e
Vladimir Starostin <vladimir.starostin@uni-tuebingen.de>
2021-07-21 11:33:04 +0200

    add major functionality

--- src/plt_pack/project_utils/plt_project.py
+++ src/plt_pack/project_utils/plt_project.py
@@ -2,58 +2,95 @@ import logging
 from pathlib import Path
 from datetime import datetime as dt
 from functools import wraps
-from typing import List, Dict, Tuple, Any, Optional
+from typing import List, Dict, Tuple, Any, Optional, Union
 from types import FunctionType
 from contextlib import contextmanager
 
 import matplotlib.pyplot as plt
 
-from .save_opts import SaveOpts, PLT_FORMAT
-from .registered_func import RegisteredFunc
+from .save_opts import SaveOpts, PLT_FORMAT, add_opt_docs
 from ..file import save_plt_file, read_plt_file, PltFile
 
 
+@add_opt_docs
 class PltProject(object):
+
+    """
+    Args:
+        folder (Path/str): A base folder of a new project. All the files will be saved to this folder.
+        save_opts (SaveOpts, optional): Default save options. Can be provided as an object or using the keyword 
+            arguments from below. 
+        {save_opt_args}
+    """
+
     _TEST: bool = False
 
     def __init__(self,
                  folder: Path or str,
-                 save_opts: SaveOpts = None,
+                 save_opts: Optional[SaveOpts] = None,
                  **save_opt_kwargs,
                  ):
         self.folder = Path(folder)
         self.folder.mkdir(exist_ok=True)
 
-        self.default_opts = save_opts or SaveOpts(**save_opt_kwargs)
-        self.current_opts: SaveOpts = None
-        self.registered_functions: Dict[str, RegisteredFunc] = {}
+        self.default_opts: SaveOpts = save_opts or SaveOpts(**save_opt_kwargs)
+        self._context_opts: SaveOpts = SaveOpts()
 
-    def _get_save_opts(self, **kwargs) -> SaveOpts:
-        opts = self.current_opts or self.default_opts
-        return opts.new_opts(**kwargs)
+    @property
+    def context_opts(self) -> Optional[SaveOpts]:
+        return self._context_opts
+
+    @add_opt_docs
+    def get_save_opts(self, **kwargs) -> SaveOpts:
+        """
+        Args:
+            {save_opt_args}
+        """
+        return self.context_opts.\
+            new_opts(only_missing_keys=True, **kwargs).\
+            new_opts(only_missing_keys=True, **self.default_opts)
 
     @contextmanager
+    @add_opt_docs
     def __call__(self,
                  save_opts: SaveOpts = None,
                  **save_opt_kwargs
                  ):
+        """
+        Args:
+            folder (Path/str): A base folder of a new project. All the files will be saved to this folder.
+            save_opts (SaveOpts, optional): Default save options. Can be provided as an object or using the keyword 
+                arguments from below. 
+        {save_opt_args}
+        """
 
-        self.current_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
+        self._context_opts = save_opts or SaveOpts(**save_opt_kwargs)
         try:
             yield
         finally:
-            self.current_opts = None
+            self._context_opts = SaveOpts()
 
+    @add_opt_docs
     def save(self,
              func: FunctionType,
-             args: Optional[Tuple[Any, ...]],
+             args: Optional[Tuple[Any, ...]] = (),
              kwargs: Optional[Dict[str, Any]] = None,
              save_opts: Optional[SaveOpts] = None,
              **save_opt_kwargs,
              ) -> Path:
 
-        save_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
-
+        """
+        Save a function and its arguments based on provided save options.
+        
+        Args:
+            func (FunctionType): Function to save.
+            args (tuple, optional): Positional arguments.
+            kwargs (dict, optional): Keyword arguments.
+            save_opts (SaveOpts, optional): Current save options. Can be provided as an object or 
+                using the keyword arguments from below.
+            {save_opt_args}
+        """
+        save_opts = self.get_save_opts(**(save_opts or save_opt_kwargs))
         plt_name, fig_name = save_opts.get_names(func)
         plt_path = self.folder / plt_name
 
@@ -67,20 +104,6 @@ class PltProject(object):
 
         return plt_path
 
-    def auto_save(self, save_opts: Optional[SaveOpts] = None, **save_opt_kwargs):
-        save_opts = save_opts or self._get_save_opts(**save_opt_kwargs)
-
-        def decorator(func):
-            @wraps(func)
-            def wrapper(*args, **kwargs):
-                if not self._TEST:
-                    func(*args, **kwargs)
-                self.save(func, args, kwargs, save_opts)
-
-            return wrapper
-
-        return decorator
-
     def register(self, func: FunctionType):
         @wraps(func)
         def wrapper(*args, **kwargs):
@@ -88,9 +111,29 @@ class PltProject(object):
 
         return wrapper
 
-    def _register_called(self, func: FunctionType, args: List[Any], kwargs: Dict[str, Any]):
-        func(*args, **kwargs)
-        self.save(func, args, kwargs)
+    @add_opt_docs
+    def auto_save(self, save_opts: Optional[SaveOpts] = None, **save_opt_kwargs):
+        """
+        A decorator that registers a decorated function with
+        Args:
+            save_opts:
+            {save_opt_args}
+
+        Returns:
+
+        """
+        default_save_opts: SaveOpts = save_opts or SaveOpts(**save_opt_kwargs)
+
+        def decorator(func):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                if not self._TEST:
+                    func(*args, **kwargs)
+                self.save(func, args, kwargs, **default_save_opts)
+
+            return wrapper
+
+        return decorator
 
     def list_files(self) -> List[str]:
         return [path.stem for path in self.folder.glob(f'*.{PLT_FORMAT}')]
@@ -100,6 +143,12 @@ class PltProject(object):
 
     def __getitem__(self, name: str):
         return self.load_file(name)
+
+    def _register_called(self, func: FunctionType, args: List[Any], kwargs: Dict[str, Any]):
+        if not self._TEST:
+            func(*args, **kwargs)
+        if self._context_opts:
+            self.save(func, args, kwargs)
 
 
 def _save_figure(
